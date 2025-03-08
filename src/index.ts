c421e1c1df977b0c2d220084c26d7c3a07eb3dea
Kotaro Yabe <tarosa@notespace.jp>
2025-03-08 22:55:00 +0900

    fixed package structure

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,32 @@
 import { Conf } from './lib/conf'
 import { payPayRestSDK, PayPayRestSDK } from './lib/paypay-rest-sdk'
 
-export = {
-    Configure: payPayRestSDK.configure,
-    CreatePayment: payPayRestSDK.createPayment,
-    QRCodeCreate: payPayRestSDK.qrCodeCreate,
-    QRCodeDelete: payPayRestSDK.qrCodeDelete,
-    GetCodePaymentDetails: payPayRestSDK.getCodePaymentDetails,
-    GetPaymentDetails: payPayRestSDK.getPaymentDetails,
-    PaymentCancel: payPayRestSDK.paymentCancel,
-    PaymentAuthCapture: payPayRestSDK.paymentAuthCapture,
-    PaymentAuthRevert: payPayRestSDK.paymentAuthRevert,
-    PaymentRefund: payPayRestSDK.paymentRefund,
-    PaymentPreauthorize: payPayRestSDK.paymentPreauthorize,
-    GetRefundDetails: payPayRestSDK.getRefundDetails,
-    CheckUserWalletBalance: payPayRestSDK.checkUserWalletBalance,
-    Authorization: payPayRestSDK.authorization,
-    AuthorizationResult: payPayRestSDK.authorizationResult,
-    AccountLinkQRCodeCreate: payPayRestSDK.accountLinkQRCodeCreate,
-    ValidateJWT: payPayRestSDK.validateJWT,
-    CreateSubscriptionPayment: payPayRestSDK.paymentSubscription,
-    CreatePendingPayment: payPayRestSDK.createPendingPayment,
-    GetPendingPaymentDetails: payPayRestSDK.getPendingPaymentDetails,
-    CancelPendingOrder: payPayRestSDK.cancelPendingOrder,
-    RefundPendingPayment: payPayRestSDK.refundPendingPayment,
-    GetUserAuthorizationStatus: payPayRestSDK.getUserAuthorizationStatus,
-    UnlinkUser: payPayRestSDK.unlinkUser,
-    CashBack: payPayRestSDK.cashBack,
-    CheckCashBackDetails: payPayRestSDK.getCashBackDetails,
-    ReversalCashBack: payPayRestSDK.reverseCashBack,
-    CheckCashBackReversalDetails: payPayRestSDK.getReverseCashBackDetails,
-    PayPayRestSDK,
-    Conf,
-}
+export const Configure = payPayRestSDK.configure
+export const CreatePayment = payPayRestSDK.createPayment
+export const QRCodeCreate = payPayRestSDK.qrCodeCreate
+export const QRCodeDelete = payPayRestSDK.qrCodeDelete
+export const GetCodePaymentDetails = payPayRestSDK.getCodePaymentDetails
+export const GetPaymentDetails = payPayRestSDK.getPaymentDetails
+export const PaymentCancel = payPayRestSDK.paymentCancel
+export const PaymentAuthCapture = payPayRestSDK.paymentAuthCapture
+export const PaymentAuthRevert = payPayRestSDK.paymentAuthRevert
+export const PaymentRefund = payPayRestSDK.paymentRefund
+export const PaymentPreauthorize = payPayRestSDK.paymentPreauthorize
+export const GetRefundDetails = payPayRestSDK.getRefundDetails
+export const CheckUserWalletBalance = payPayRestSDK.checkUserWalletBalance
+export const Authorization = payPayRestSDK.authorization
+export const AuthorizationResult = payPayRestSDK.authorizationResult
+export const AccountLinkQRCodeCreate = payPayRestSDK.accountLinkQRCodeCreate
+export const ValidateJWT = payPayRestSDK.validateJWT
+export const CreateSubscriptionPayment = payPayRestSDK.paymentSubscription
+export const CreatePendingPayment = payPayRestSDK.createPendingPayment
+export const GetPendingPaymentDetails = payPayRestSDK.getPendingPaymentDetails
+export const CancelPendingOrder = payPayRestSDK.cancelPendingOrder
+export const RefundPendingPayment = payPayRestSDK.refundPendingPayment
+export const GetUserAuthorizationStatus = payPayRestSDK.getUserAuthorizationStatus
+export const UnlinkUser = payPayRestSDK.unlinkUser
+export const CashBack = payPayRestSDK.cashBack
+export const CheckCashBackDetails = payPayRestSDK.getCashBackDetails
+export const ReversalCashBack = payPayRestSDK.reverseCashBack
+export const CheckCashBackReversalDetails = payPayRestSDK.getReverseCashBackDetails
+export { PayPayRestSDK, Conf }
